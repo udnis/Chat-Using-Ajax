675667750555a8c3e38e35b695f0df25c937e9b8
Sinduja <sinduja.nasa01@gmail.com>
2018-02-16 16:25:54 +0530

    Add files via upload

---
+++ getuser.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+
+
+$conn = mysqli_connect('localhost','root','','chats');
+if (!$conn) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+
+$sql="SELECT message FROM udapp ";
+$result = $conn->query($sql);
+
+if (1) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["message"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</body>
+</html>
